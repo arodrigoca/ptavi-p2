79c983931d9a2077675ba295c242cae173e08431
Adrian Rodrigo Castillo <arodrigo@alpha07.aulas.gsyc.urjc.es>
2017-09-26 14:32:24 +0200

    added calculator program built with objects

---
+++ CALCOBJ/CALCOBJ.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculator ():
+
+    def __init__(self):
+        pass
+
+    def plus(self, oper1, oper2):
+        return int(oper1) + int(oper2)
+
+    def minus(self, oper1, oper2):
+        return int(oper1) - int(oper2)
+
+#If   this is executed on his own, do this:
+if __name__ == "__main__":
+    try:
+        operand1 = int(sys.argv[1])
+        operand2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit('Error: operands must be integer numbers')
+
+    calc = Calculator()
+
+    if sys.argv[2] == 'plus':
+        result = calc.plus(sys.argv[1],sys.argv[3])
+    elif sys.argv[2] == 'minus':
+        result = calc.minus(sys.argv[1],sys.argv[3])
+    else:
+        sys.exit('Only Add or Substract operations are allowed')
+    print(result)
