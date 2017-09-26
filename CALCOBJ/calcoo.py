965065cf321cc2c33104c2861a67f9a174bbfd38
Adrian Rodrigo Castillo <arodrigo@alpha07.aulas.gsyc.urjc.es>
2017-09-26 15:43:42 +0200

    Added more archives

---
+++ CALCOBJ/calcoo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculator ():
+
+    def __init__(self):
+        pass
+
+    def plus(self, oper1, oper2):
+        return int(oper1) + int(oper2)
+
+    def minus(self, oper1, oper2):
+        return int(oper1) - int(oper2)
+
+#If   this is executed on his own, do this:
+
+def Operate():
+
+    if sys.argv[2] == 'plus':
+        result = calc.plus(sys.argv[1],sys.argv[3])
+    elif sys.argv[2] == 'minus':
+        result = calc.minus(sys.argv[1],sys.argv[3])
+    else:
+        print()
+        sys.exit('Error: Only Add or Substract operations are allowed')
+    return result
+
+
+
+if __name__ == "__main__":
+
+    print()
+    print('//----Adrián Rodrigo Castillo, 3o ISAM URJC-------')
+    print('//----Simple calculator program')
+    print()
+    print('Input operand was: ' + str(sys.argv[2]) + ' and operands were: ' + str(sys.argv[1]) + ' and ' + str(sys.argv[3]))
+    try:
+        operand1 = int(sys.argv[1])
+        operand2 = int(sys.argv[3])
+    except ValueError:
+        print()
+        sys.exit('Error: operands must be integer numbers')
+
+    calc = Calculator()
+    result = Operate()
+    print()
+    print('Result is: ' + str(result))
+    print()
